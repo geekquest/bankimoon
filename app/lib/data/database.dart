7519937319a49c71088cbbf2906bc6c1995fa5d1
Zikani Nyirenda Mwase <zikani.nmwase@ymail.com>
2023-09-07 03:21:50 +0200

    feat: Implemented favourites functionality and screen closes #25
    Added a page to show favourited accounts, along with the
    accompanying functionality to mark an account as a favourite.
    Currently there isn't a way to "unfavourite" an account.
    Also needed to add a migration to make the functionality work.
    
    Refactored a bit of the code where accounts were being mapped.

--- app/lib/data/database.dart
+++ app/lib/data/database.dart
@@ -12,6 +12,12 @@ dynamic encrypter(String password) {
   return encrypted;
 }
 
+
+/// Update Company table V1 to V2
+void _addIsFavouriteToAccountsTableV2(Batch batch) {
+  batch.execute('ALTER TABLE accounts ADD isFavourite boolean default FALSE;');
+}
+
 class DbManager {
   // ignore: unused_field
   late Database _database;
@@ -19,14 +25,23 @@ class DbManager {
   Future openDb() async {
     _database = await openDatabase(
         join(await getDatabasesPath(), "bankimoon.db"),
-        version: 1, onCreate: (Database db, int version) async {
-      await db.execute(
-        "CREATE TABLE IF NOT EXISTS accounts(id INTEGER PRIMARY KEY autoincrement, bankName TEXT, accountName TEXT, accountNumber INTEGER)",
-      );
-      await db.execute(
-        "CREATE TABLE IF NOT EXISTS password(id INTEGER PRIMARY KEY autoincrement, password TEXT)",
-      );
-    });
+        version: 2, 
+        onCreate: (Database db, int version) async {
+          await db.execute(
+            "CREATE TABLE IF NOT EXISTS accounts(id INTEGER PRIMARY KEY autoincrement, bankName TEXT, accountName TEXT, accountNumber INTEGER)",
+          );
+          await db.execute(
+            "CREATE TABLE IF NOT EXISTS password(id INTEGER PRIMARY KEY autoincrement, password TEXT)",
+          );
+        },
+        onUpgrade: (db, oldVersion, newVersion) async {
+          var batch = db.batch();
+          if (oldVersion == 1) {
+            _addIsFavouriteToAccountsTableV2(batch);
+          }
+          await batch.commit();
+        },
+    );
     return _database;
   }
 
@@ -110,5 +125,41 @@ class DbManager {
     final results = await _database.rawQuery(sql, params);
 
     return results;
+  }
+
+  Future getFavourites() async {
+    await openDb();
+
+    String sql = "SELECT * FROM accounts WHERE isFavourite = 1";
+
+    final results = await _database.rawQuery(sql, []);
+
+    return results;
+  }
+
+  Future searchFavouriteAccounts(String searchQuery) async {
+    await openDb();
+
+    String sql =
+        "SELECT * FROM accounts WHERE isFavourite = 1 AND (accountName LIKE ? OR accountNumber LIKE ?)";
+
+    List<dynamic> params = [
+      '%${searchQuery.trim()}%',
+      '%${searchQuery.trim()}%'
+    ];
+
+    final results = await _database.rawQuery(sql, params);
+
+    return results;
+  }
+
+  Future markAsFavourite(int accountId) async {
+    await openDb();
+
+    String sql = "UPDATE accounts SET isFavourite = 1 WHERE id = ?";
+
+    List<dynamic> params = [ accountId ];
+
+    return await _database.rawQuery(sql, params);
   }
 }
--- app/lib/utils/router.dart
+++ app/lib/utils/router.dart
@@ -2,6 +2,7 @@ import 'package:bankimoon/data/cubit/accounts_cubit.dart';
 import 'package:bankimoon/data/database.dart';
 import 'package:bankimoon/data/repo.dart';
 import 'package:bankimoon/presentation/screens/add_account.dart';
+import 'package:bankimoon/presentation/screens/favourites.dart';
 import 'package:bankimoon/presentation/screens/splash_screen.dart';
 import 'package:bankimoon/presentation/screens/home.dart';
 import 'package:bankimoon/utils/constants.dart';
@@ -29,6 +30,13 @@ class AppRouter {
           builder: (_) => BlocProvider(
             create: (context) => AccountsCubit(repository: repository),
             child: const Home(),
+          ),
+        );
+      case favouritePage:
+        return MaterialPageRoute(
+          builder: (_) => BlocProvider(
+            create: (context) => AccountsCubit(repository: repository),
+            child: const Favourite(),
           ),
         );
       case addAccount:
