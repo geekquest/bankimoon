e234821fc48d0bb60a213e74c38e134114ae38e2
Steve Chikwiri <37661480+mijiga@users.noreply.github.com>
2024-01-03 15:11:54 +0200

    Isar repo setup

--- app/lib/data/isar_repo.dart
+++ app/lib/data/isar_repo.dart
@@ -4,15 +4,88 @@ import 'package:path_provider/path_provider.dart';
 
 class IsarRepo {
   IsarRepo._();
+
   static final IsarRepo _instance = IsarRepo._();
+
   static IsarRepo get instance => _instance;
 
   late Isar isarInstance;
 
-  init () async {
+  init() async {
     final dir = await getApplicationDocumentsDirectory();
     isarInstance = await Isar.open([AccountSchema], directory: dir.path);
   }
 
+  Future<List<Account>> getAccounts() async {
+    final data = await isarInstance.accounts.where().findAll();
+    return data.map((e) => Account.fromJson(e.toJson())).toList();
+  }
 
+  Future addAccount(
+      String institutionName, String accountName, String accountNumber) async {
+    return await isarInstance.writeTxn(() async {
+      var data = await isarInstance.accounts.put(Account(
+        bankName: institutionName,
+        accountName: accountName,
+        accountNumber: int.parse(accountNumber),
+        isFavourite: false,
+      ));
+      return data;
+    });
+  }
+
+  deleteAccount(int id) async {
+    await isarInstance
+        .writeTxn(() async => await isarInstance.accounts.delete(id));
+  }
+
+  deleteAccounts() async {
+    await isarInstance
+        .writeTxn(() async => await isarInstance.accounts.deleteAll([]));
+  }
+
+  Future<List<Account>> getFavourites() async {
+    final data = await isarInstance.accounts
+        .where()
+        .filter()
+        .isFavouriteEqualTo(true)
+        .findAll();
+    return data.map((e) => Account.fromJson(e.toJson())).toList();
+  }
+
+  markAsFavourite(int accountId) async {
+    await isarInstance.writeTxn(() async {
+      final account = await isarInstance.accounts.get(accountId);
+      account!.isFavourite = true;
+      await isarInstance.accounts.put(account);
+    });
+  }
+
+  removeAsFavourite(int accountId) async {
+    await isarInstance.writeTxn(() async {
+      final account = await isarInstance.accounts.get(accountId);
+      account!.isFavourite = false;
+      await isarInstance.accounts.put(account);
+    });
+  }
+
+  Future<List<Account>> searchAccount(String query) async {
+    final data = await isarInstance.accounts
+        .where()
+        .filter()
+        .accountNameContains(query)
+        .findAll();
+    return data.map((e) => Account.fromJson(e.toJson())).toList();
+  }
+
+  searchFavouriteAccounts(String query) async {
+    final data = await isarInstance.accounts
+        .where()
+        .filter()
+        .accountNameContains(query)
+        .and()
+        .isFavouriteEqualTo(true)
+        .findAll();
+    return data.map((e) => Account.fromJson(e.toJson())).toList();
+  }
 }
